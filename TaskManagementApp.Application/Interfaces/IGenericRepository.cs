b372e33025d539c845a0c6fdadd6d54a6fa64e27
Ajani Opeyemi <ooajani@outlook.com>
2022-11-12 12:57:57 +0100

    Add project files.

---
+++ TaskManagementApp.Application/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApp.Application.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> Get(int id);
+        Task<IEnumerable> GetAll();
+        Task<int> Add(T entity);
+        Task<int> Delete(int id);
+        Task<int> Update(T entity);
+
+    }
+}
---
+++ TaskManagementApp.Application/Tasks/Validators/CreateTaskCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagementApp.Application.Tasks.Commands;
+
+namespace TaskManagementApp.Application.Tasks.Validators
+{
+    public class CreateTaskCommandValidator : AbstractValidator<CreateTaskCommand>
+    {
+        public CreateTaskCommandValidator()
+        {
+            RuleFor(t => t.Name).NotEmpty();
+            RuleFor(t => t.Description).NotEmpty();
+            RuleFor(t => t.Status).NotNull();
+            RuleFor(t => t.DueDate).NotNull();
+        }
+    }
+}
